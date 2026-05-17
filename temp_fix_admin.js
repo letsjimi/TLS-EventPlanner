ed556be83f5ca1af1aee8dd66bfdbf6c00f06cc0
Timon <timon@tls-livesound.de>
2026-05-17 03:25:10 +0200

    auto: cleanup temp files from previous commit

--- temp_fix_admin.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const bcrypt = require('bcryptjs');
-const db = new sqlite3.Database('./data/tms.db');
-const hash = bcrypt.hashSync('admin', 10);
-db.run('UPDATE users SET password_hash = ? WHERE username = ?', [hash, 'admin'], function(err) {
-  if (err) { console.error(err); process.exit(1); }
-  console.log('updated rows:', this.changes);
-  process.exit(0);
-});
